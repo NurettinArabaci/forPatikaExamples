85bcde0b127db3a17113bf14a84cf85522a63be9
NurettinArabaci <91418187+NurettinArabaci@users.noreply.github.com>
2021-11-13 00:44:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consoleders
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("selam");
+        }
+    }
+}
